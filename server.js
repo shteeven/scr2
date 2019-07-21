3aab25aec905caa090a7e6e3eec738eaa58339cb
Steven Barnhurst <stevenbarnhurst@gmail.com>
2019-07-20 20:51:33 -0500

    Update static server

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+//
+// app.use(express.static(__dirname + '/dist'));
+//
+// app.listen(process.env.PORT || 8080);
+//
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/scradio/index.html'));
+// });
+//
+// console.log('Console listening!');
+
+'use strict';
 const express = require('express');
+const compression = require('compression');
+
+const _port = 4100;
+const _app_folder = 'dist/scradio';
+
 const app = express();
-const path = require('path');
+app.use(compression());
 
-app.use(express.static(__dirname + '/dist'));
+// ---- SERVE STATIC FILES ---- //
+app.get('*.*', express.static(_app_folder, { maxAge: '1y' }));
 
-app.listen(process.env.PORT || 8080);
-
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+// ---- SERVE APLICATION PATHS ---- //
+app.all('*', function(req, res) {
+  res.status(200).sendFile(`/`, { root: _app_folder });
 });
 
-console.log('Console listening!');
+// ---- START UP THE NODE SERVER  ----
+app.listen(_port, function() {
+  console.log('Node Express server for ' + app.name + ' listening on http://localhost:' + _port);
+});
