b77ebe9f2fbb7498d2ce3c00ff345385d01a2b03
Steven Barnhurst <stevenbarnhurst@gmail.com>
2019-11-10 15:02:38 -0600

    Add Interviews list
    Also refactor landing component to be
    cleaner and remove pre-commit lint
    check.
    
    scr-6

--- src/app/pages/landing/landing/landing.component.ts
+++ src/app/pages/landing/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MediaService } from '../../../services/media.service';
 import { VideoPlayerService } from '../../../shared/video-player/video-player.service';
 
@@ -7,17 +7,12 @@ import { VideoPlayerService } from '../../../shared/video-player/video-player.se
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent implements OnInit {
-  videos$;
-  featured$;
-  limit;
+export class LandingComponent {
+  videos$ = this.mediaService.getVideoList();
+  featured$ = this.mediaService.getPlaylist('x6gd0m');
+  interviews$ = this.mediaService.getPlaylist('x6iutn');
 
-  constructor(private mediaService: MediaService, private videoPlayer: VideoPlayerService) {
-    this.videos$ = mediaService.getVideoList();
-    this.featured$ = mediaService.getPlaylist();
-  }
-
-  ngOnInit() {}
+  constructor(private mediaService: MediaService, private videoPlayer: VideoPlayerService) {}
 
   startStream(video) {
     this.videoPlayer.open(video);
